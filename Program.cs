430f0d567ae2e7e10727cedbc957bde107e3fd55
deymusis <vulpinari@yandex.ru>
2018-08-24 17:48:16 +0300

    Hellish World
    Signed-off-by: deymusis <vulpinari@yandex.ru>

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellishWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Консольное приложение*/
+            Console.WriteLine("Hellish World!"); // Гори оно огнём!
+            Console.ReadLine(); // После Enter окно закроется
+            /*F5 запустить код*/
+        }
+    }
+}
